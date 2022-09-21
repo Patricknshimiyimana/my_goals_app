9a2e485b0a678ddd41691315848d85c861128faa
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-09-21 17:17:56 +0200

    managing list data

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   const [goalInput, setGoalInput] = useState("");
+  const [goals, setGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
-    setGoalInput(enteredText)
-  }
+    setGoalInput(enteredText);
+  };
 
   const addGoalHandler = () => {
-    console.log(goalInput)
-  }
+    setGoals((currentGoals) => [...currentGoals, goalInput]);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goals' onChangeText={goalInputHandler} />
-        <Button title='Add goal' onPress={addGoalHandler}/>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goals"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals</Text>
-        
+        {goals.map((goal) => {
+          return <Text key={goal}>{goal}</Text>
+        })}
       </View>
     </View>
   );
@@ -30,25 +37,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 80,
-    paddingHorizontal: 26
+    paddingHorizontal: 26,
   },
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    marginBottom: 24
+    borderBottomColor: "#ccc",
+    marginBottom: 24,
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#ccc',
-    width: '70%',
+    borderColor: "#ccc",
+    width: "70%",
     padding: 8,
-    marginRight: 8
+    marginRight: 8,
   },
   goalsContainer: {
     flex: 5,
-  }
+  },
 });
