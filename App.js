dc42c843ec7af2db90e315336c4af816652689b5
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-09-26 10:24:19 +0200

    deleting an item & press effects

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ export default function App() {
   };
 
   const deleteGoalHandler = (id) => {
-    console.log("DELETE ITEM HERE!");
     setGoals((currentGoals) => {
       return currentGoals.filter((goal) => goal.id !== id);
     });
